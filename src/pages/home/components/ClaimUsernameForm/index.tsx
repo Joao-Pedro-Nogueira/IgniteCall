b9108fdc5fcf00ea2bc936056771c74c42bf937f
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2023-03-02 22:58:25 -0400

    HomePage

---
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -0,0 +1,54 @@
+import { Button, Text, TextInput } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Form, FormAnnotation } from './styles'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'Deve ter no mínimo 3 caracteres' })
+    .regex(/^([a-z\\-]+)$/i, { message: 'Deve ter apenas letras e/ou hifens' })
+    .transform((username) => username.toLocaleLowerCase()),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+
+export function ClaimUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
+  return (
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="calendar.io/"
+          placeholder="seu-usuario"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit">
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username.message
+            : 'Digite seu nome de usuário'}
+        </Text>
+      </FormAnnotation>
+    </>
+  )
+}
