10e6c41fa65a3fad145ded6c1264c3ae37b7b2d6
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2023-03-04 18:24:36 -0400

    ConnectCalendarPage

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,37 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { Container, Header } from '../styles'
+import { ConnectBox, ConnectItem } from './styles'
+
+export default function Register() {
+  // async function handleRegister() {
+
+  // }
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Bem-vindo ao Ignite Call</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Agenda</Text>
+          <Button variant="secondary" size="sm">
+            Conectar
+            <ArrowRight></ArrowRight>
+          </Button>
+        </ConnectItem>
+        <Button type="submit">
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}
