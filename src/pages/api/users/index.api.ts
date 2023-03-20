f8d6d306aba8bf9389e716f25c55896e76dccfdd
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2023-03-20 18:39:50 -0400

    Erro permanece

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,6 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getServerSession } from 'next-auth'
+import { buildNextAuthOptions } from '../auth/[...nextauth].api'
+import { z } from 'zod'
 import { prisma } from '@/src/lib/prisma'
-import { setCookie } from 'nookies'
-import type { NextApiRequest, NextApiResponse } from 'next'
+
+const timeIntervalsBodySchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number(),
+      startTimeInMinutes: z.number(),
+      endTimeInMinutes: z.number(),
+    }),
+  ),
+})
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,31 +22,30 @@ export default async function handler(
     return res.status(405).end()
   }
 
-  const { name, username } = req.body
+  const session = await getServerSession(
+    req,
+    res,
+    buildNextAuthOptions(req, res),
+  )
 
-  const userExists = await prisma.user.findUnique({
-    where: {
-      username,
-    },
-  })
-
-  if (userExists) {
-    return res.status(400).json({
-      message: 'Username already exists',
-    })
+  if (!session) {
+    return res.status(401).end()
   }
 
-  const user = await prisma.user.create({
-    data: {
-      name,
-      username,
-    },
-  })
+  const { intervals } = timeIntervalsBodySchema.parse(req.body)
 
-  setCookie({ res }, '@ignitecall:userId', user.id, {
-    maxAge: 60 * 60 * 24 * 7, // 7 days
-    path: '/',
-  })
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          time_start_in_minutes: interval.startTimeInMinutes,
+          time_end_in_minutes: interval.endTimeInMinutes,
+          user_id: session.user?.id,
+        },
+      })
+    }),
+  )
 
-  return res.status(201).json(user)
+  return res.status(201).end()
 }
